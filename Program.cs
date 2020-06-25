26bacd6eebf7ee8b01c645a96560845e9f246628
Jan Reinhardt <Jan.Reinhardt@aitgmbh.de>
2020-06-25 22:19:09 +0200

    refactor main

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using ProgrammingChallenge2.Codecs;
-using ProgrammingChallenge2.Model;
 
 namespace ProgrammingChallenge2
 {
@@ -8,15 +7,19 @@ namespace ProgrammingChallenge2
     {
         public static void Main(string[] args)
         {
-            var source = new DataSource();
-
-            var codecFactory = new Codecs.JanReinhardt.JsonCodecFactory();
-
-            var seq = new TransmissionSequence();
-
             // 1days * 24h * 60min * 60s = 2592000
             long messageCount = 1*24*60*60;
 
+            Run(messageCount, new Codecs.JanReinhardt.JsonCodecFactory());
+        }
+
+        private static long Run(long messageCount, ICodecFactory codecFactory)
+        {
+            var source = new DataSource();
+
+            var seq = new TransmissionSequence();
+
+            Console.WriteLine();
             Console.WriteLine("Start...");
 
             var totalBytesTransmitted = seq.Run(source, codecFactory.CreateEncoder(), codecFactory.CreateDecoder(), messageCount, false);
@@ -24,6 +27,8 @@ namespace ProgrammingChallenge2
             Console.WriteLine($"Result for Codec '{codecFactory.Name}'");
             Console.WriteLine($"Total bytes transmitted: {totalBytesTransmitted}");
             Console.WriteLine(FormattableString.Invariant($"({totalBytesTransmitted/1024.0:F1}kB, {totalBytesTransmitted/(1024*1024):F1}MB)"));
+
+            return totalBytesTransmitted;
         }
     }
 }
