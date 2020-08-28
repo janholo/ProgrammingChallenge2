13e7a840523dbd5765e4985ebea5d010e56e7cf0
Jan Reinhardt <Jan.Reinhardt@aitgmbh.de>
2020-08-28 09:51:59 +0200

    Results List

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ProgrammingChallenge2.Codecs;
 
 namespace ProgrammingChallenge2
@@ -10,7 +12,30 @@ namespace ProgrammingChallenge2
             // 1days * 24h * 60min * 60s = 2592000
             long messageCount = 1*24*60*60;
 
-            Run(messageCount, new Codecs.JanReinhardt.JsonCodecFactory());
+            var codecs = new List<ICodecFactory>()
+            {
+                new Codecs.JanReinhardt.JsonCodecFactory(),
+                new Codecs.FlorianBader.AwesomeCodecFactory(),
+                new Codecs.Matthid.CustomCodecFactory(),
+                new Codecs.Matthid.ProtobufCodecFactory(),
+                new Codecs.Matthid.StupidProtobufCodecFactory(),
+                new Codecs.SebastianRoether.EntropyMaximizingCodecFactory(),
+                new Codecs.SebastianRoether.StatelessCodecFactory(),
+                //new Codecs.SebastianRoether.TotallyLegitCodecFactory(),
+            };
+
+            var results = codecs.Select(c => (name: c.Name, bytes: Run(messageCount, c)));
+
+            var sortedResults = results.OrderBy(r => r.bytes).ToArray();
+
+            Console.WriteLine("Results:");
+
+            var slowest = sortedResults.Last();
+
+            foreach(var r in sortedResults)
+            {
+                Console.WriteLine($"{r.name}: {r.bytes} Bytes  ({(double)slowest.bytes/r.bytes:F1}x)");
+            }
         }
 
         private static long Run(long messageCount, ICodecFactory codecFactory)
